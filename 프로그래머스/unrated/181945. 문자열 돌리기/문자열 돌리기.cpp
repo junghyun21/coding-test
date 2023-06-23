d27c43f1577f5e33eb4fbd06ef9c1908402fa3ea
Junghyun Lee <76541903+junghyun21@users.noreply.github.com>
2023-06-23 18:28:10 +0900

    [unrated] Title: 문자열 돌리기, Time: 5.05 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    cin >> str;
+    
+    for(char s: str)
+    {
+        cout << s << endl;
+    }
+    
+    return 0;
+}
