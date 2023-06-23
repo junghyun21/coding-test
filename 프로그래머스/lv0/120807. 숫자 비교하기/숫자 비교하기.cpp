3437de28b65b770ec5b0af24c2ccced517823697
Junghyun Lee <76541903+junghyun21@users.noreply.github.com>
2023-06-23 19:04:59 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    return num1 == num2 ? 1 : -1;
+//     int answer = 0;
+    
+//     if(num1 == num2)
+//     {
+//         answer = 1;
+//     }
+//     else
+//     {
+//         answer = -1;
+//     }
+    
+//     return answer;
+}
