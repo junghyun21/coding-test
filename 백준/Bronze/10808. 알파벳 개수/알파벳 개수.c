9925dd2a4848a59b1bdb794224a7b1be01de0ad1
Junghyun Lee <76541903+junghyun21@users.noreply.github.com>
2024-01-16 22:16:33 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define MAX_SIZE 100
+#define ALPHABET 26
+
+int main()
+{
+	int i, idx, len, alphabet[ALPHABET] = {0, };
+	char str[MAX_SIZE];
+
+	scanf("%s", str);
+
+	len = strlen(str);
+
+	for(i = 0; i < len; i++){
+		idx = str[i] > 'Z' ? str[i] - 'a' : str[i] - 'A';
+		++alphabet[idx];
+	}
+	
+	for(i = 0; i < ALPHABET; i++)
+		printf("%d ", alphabet[i]);
+	printf("\n");
+
+	return 0;
+}
