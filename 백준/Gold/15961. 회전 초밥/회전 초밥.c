da41b8f0869cf88fcda1f68f4144e5a1d16ad039
Junghyun Lee <76541903+junghyun21@users.noreply.github.com>
2024-08-01 10:21:38 +0900

    Update 회전 초밥.c

--- 백준/Gold/15961. 회전 초밥/회전 초밥.c
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 int main()
 {
@@ -9,7 +8,6 @@ int main()
     int n, d, k, c;
     int newIdx, oldIdx;
     int max, cnt;
-    bool hasCoupon;
     int i;
 
     (void)scanf("%d %d %d %d\n", &n, &d, &k, &c);
@@ -23,7 +21,7 @@ int main()
     for(i = 1; i <= d; i++)
         sushis[i] = 0;
 
-    for(i = 1, max = 0, cnt = 0, hasCoupon = false; i < n + k; i++){
+    for(i = 1, max = 0, cnt = 0; i < n + k; i++){
         oldIdx = i > k ? i - k : 0;
         if(i > n)
             newIdx = i % n;
