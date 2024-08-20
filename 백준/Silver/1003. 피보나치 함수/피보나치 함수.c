13296c5714168565d69f9293bccfd24aa038cc86
Junghyun Lee <76541903+junghyun21@users.noreply.github.com>
2024-08-20 15:51:59 +0900

    [Silver III] Title: 피보나치 함수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.c
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.c
@@ -3,48 +3,32 @@
 
 #define MAX_TEST 40 + 1
 
-int dp[MAX_TEST][2]; // 0과 1을 호출한 횟수를 저장할 배열
 
 void fibonacci(int n);
 
 int main()
 {
+    int dp[MAX_TEST][2]; // 0과 1을 호출한 횟수를 저장할 배열
     int T; // 테스트 케이스 개수
-    int n;
+    int n; // 테스트 케이스
+    int i;
 
     scanf("%d", &T);
 
+    dp[0][0] = 1;
+    dp[0][1] = 0;
+    dp[1][0] = 0;
+    dp[1][1] = 1;
+
+    for(i = 2; i < MAX_TEST; i++){
+        dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+        dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+    }
+
     while (T--){
         scanf("%d", &n);
-
-        fibonacci(n);
-        
-        // f(n)인 경우
         printf("%d %d\n", dp[n][0], dp[n][1]);
     }
     
     return 0;
-}
-
-void fibonacci(int n)
-{
-    // 0열과 1열 중 하나라도 0이 아닌 경우: 값이 채워진 경우
-    if(dp[n][0] || dp[n][1])
-        return;
-
-    if(n == 0){
-        dp[n][0] = 1;
-    }
-    else if(n == 1){
-        dp[n][1] = 1;
-    }
-    else{
-        fibonacci(n - 1);
-        fibonacci(n - 2);
-
-        dp[n][0] = dp[n - 1][0] + dp[n - 2][0];
-        dp[n][1] = dp[n - 1][1] + dp[n - 2][1];
-    }
-
-    return;
 }
