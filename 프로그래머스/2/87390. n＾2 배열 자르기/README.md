d05307e1b04363ed0e2f257f6faa34869869dd75
Junghyun Lee <76541903+junghyun21@users.noreply.github.com>
2025-03-04 13:29:21 +0900

    [level 2] Title: n^2 배열 자르기, Time: 110.37 ms, Memory: 69.6 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/README.md
+++ 프로그래머스/2/87390. n＾2 배열 자르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] n^2 배열 자르기 - 87390 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87390) 
 
 ### 성능 요약
 
-메모리: 69.7 MB, 시간: 106.54 ms
+메모리: 69.6 MB, 시간: 110.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 04일 13:21:35
+2025년 03월 04일 13:29:17
 
 ### 문제 설명
 
--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.cpp
@@ -2,13 +2,17 @@
 
 using namespace std;
 
+// n*n 크기의 1차원 배열 생성 및 초기화 시, 메모리/시간 초과 발생!!
+// 처음부터 push_back()을 통해 right - left + 1 크기의 답안(answer)을 생성하자
 vector<int> solution(int n, long long left, long long right) {
     vector<int> answer;
     vector<int> x;
     
+    // {1, ..., n}의 배열 생성
     for(int i = 1; i <= n; ++i) 
         x.push_back(i);
     
+    // n*n 크기의 1차원 배열(arr) 중, arr[left] ... arr[right] 값만 저장
     for(long long i = left; i <= right; ++i) {
         int row = i / n;
         int col = i % n;
