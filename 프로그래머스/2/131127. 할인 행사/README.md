00df284fa7b6b901921e888807a2fee7851c057a
Junghyun Lee <76541903+junghyun21@users.noreply.github.com>
2025-04-04 00:57:10 +0900

    [level 2] Title: 할인 행사, Time: 69.19 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/README.md
+++ 프로그래머스/2/131127. 할인 행사/README.md
@@ -1,10 +1,10 @@
 # [level 2] 할인 행사 - 131127 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131127) 
 
 ### 성능 요약
 
-메모리: 12 MB, 시간: 7.96 ms
+메모리: 12 MB, 시간: 69.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 04일 00:30:52
+2025년 04월 04일 00:57:06
 
 ### 문제 설명
 
--- 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.cpp
@@ -1,6 +1,5 @@
 #include <string>
 #include <vector>
-#include <queue>
 #include <unordered_map>
 
 using namespace std;
@@ -10,49 +9,18 @@ int solution(vector<string> want, vector<int> number, vector<string> discount) {
     
     unordered_map<string, int> umap; // <물품 이름(want[i]), 물품 개수(number[i])>
     
-    for(int i = 0; i < want.size(); ++i) {
-        umap.insert({want[i], number[i]});
-    }
-    
-    queue<string> items;
+    for(int i = 0; i < want.size(); ++i)
+        umap[want[i]] = number[i];
    
-    for(int i = 0; i < discount.size(); ++i) {
-        // 현재 물품이 구매할 항목에 포함되지 않는 경우: 장바구니 목록 모두 삭제
-        if(umap.find(discount[i]) == umap.end()) {
-            while(!items.empty()) {
-                ++umap[items.front()];
-                items.pop();
-            }
-            
-            continue;
-        }
+    for(int i = 0; i < discount.size() - 9; ++i) {
+        // i일에 회원가입 시에 할인받을 수 있는 품목
+        unordered_map<string, int> discount_umap;
         
-        // 장바구니가 가득 차지 않은 경우
-        if(items.size() < 10) {
-            items.push(discount[i]);
-            --umap[discount[i]];
-        }
-        // 장바구니가 가득 찬 경우
-        else{
-            ++umap[items.front()];
-            items.pop();
-            
-            items.push(discount[i]);
-            --umap[discount[i]];
-        }
+        for(int j = i; j < 10 + i; ++j)
+            ++discount_umap[discount[j]];
         
-        // 초과구매한 물품이 있는지 확인
-        if(items.size() == 10) {
-            bool hasNegative = false;
-            for (const auto& [key, value] : umap) {
-                if (value < 0) {
-                    hasNegative = true;
-                    break;
-                }
-            }
-            
-            if(!hasNegative) ++answer;
-        }
+        if(umap == discount_umap) 
+            ++answer;
     }
     
     return answer;
