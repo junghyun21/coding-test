e4ba9d675a3788a02c6f653402c632d8e506e0b8
Junghyun Lee <76541903+junghyun21@users.noreply.github.com>
2025-04-03 23:05:03 +0900

    [level 2] Title: 전화번호 목록, Time: 78.71 ms, Memory: 35.6 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,24 +1,15 @@
 #include <string>
 #include <vector>
-#include <unordered_set>
+#include <algorithm>
 
 using namespace std;
 
 bool solution(vector<string> phone_book) {
-    vector<unordered_set<string>> numbers(21); // 길이 1 ~ 20
+    sort(phone_book.begin(), phone_book.end());
     
-    // numbers[번호의 길이]
-    for(const auto &p : phone_book)
-        numbers[p.size()].insert(p);
-    
-    // phone_book의 요소들 하나씩 검사
-    for(int i = 0; i < phone_book.size(); ++i) {  
-        // 앞부터 한글자, 두글자, ... 가 다른 번호와 동일한지 확인
-        for(int j = 1; j < phone_book[i].size(); ++j) {
-            string temp = phone_book[i].substr(0, j);
-            if(numbers[j].find(temp) != numbers[j].end())
-                return false;
-        }
+    for(int i = 1; i < phone_book.size(); ++i) {
+        if(phone_book[i - 1] == phone_book[i].substr(0, phone_book[i - 1].size()))
+            return false;
     }
     
     return true;
