3799542480709852e22aeb3c2d4ba0651ea8bd5e
Junghyun Lee <76541903+junghyun21@users.noreply.github.com>
2026-04-10 23:43:17 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.15 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.cpp
@@ -1,76 +1,67 @@
 #include <vector>
-#include <limits>
 #include <queue>
+#include <limits>
 #include <algorithm>
 
 using namespace std;
 
-const int INF = numeric_limits<int>::max();
-const int dx[4] = {0, 0, -1, 1};
-const int dy[4] = {-1, 1, 0, 0};
+const int dr[] = {1, -1, 0, 0};
+const int dc[] = {0, 0, 1, -1};
 
 typedef struct Node {
-    int x;
-    int y;
-    int cost;
-    int direct;
+    int r;
+    int c;
+    int d;
 } Node;
 
 int solution(vector<vector<int>> board) {
-    int N = board.size();
+    int cost[27][27][4];
+    int map[27][27];
+    int n = board.size();
     
-    int new_board[N + 2][N + 2];
-    int cost[4][N + 2][N + 2]; // 0: 위, 1: 아래, 2: 왼쪽, 3: 오른쪽
+    fill(&cost[0][0][0], &cost[0][0][0] + 27 * 27 * 4, -1);
+    fill(&map[0][0], &map[0][0] + 27 * 27, 1);
     
-    fill(&new_board[0][0], &new_board[0][0] + (N + 2) * (N + 2), 1);
-    fill(&cost[0][0][0], &cost[0][0][0] + 4 * (N + 2) * (N + 2), INF);
-    
-    for(int i = 0; i < N; ++i) {
-        for(int j = 0; j < N; ++j) {
-            new_board[i + 1][j + 1] = board[i][j];
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n; ++j) {
+            map[i + 1][j + 1] = board[i][j];
         }
     }
-
-    queue<Node> q;
-    q.push({1, 1, 0, 3}); // 오른쪽 방향으로 출발
-    q.push({1, 1, 0, 1}); // 아래 방향으로 출발
     
-    for(int i = 0; i < 4; ++i) {
-        cost[i][1][1] = 0;
-    }
+    queue<Node> q;
+    q.push({1, 1, 0});
+    q.push({1, 1, 2});
+    
+    cost[1][1][0] = 0;
+    cost[1][1][2] = 0;
     
     while(!q.empty()) {
-        Node curr = q.front();
+        auto [r, c, d] = q.front();
         q.pop();
         
-        // 도착지라면 더 이상 경로 탐색하지 않음
-        if(curr.x == N && curr.y == N) {
-            continue;
-        }
-        
-        // 좌,우,위,아래 방향으로 경로 탐색
         for(int i = 0; i < 4; ++i) {
-            int new_x = curr.x + dx[i];
-            int new_y = curr.y + dy[i];
-            int new_cost = (curr.direct == i) ? curr.cost + 100 : curr.cost + 600;
+            int new_r = r + dr[i];
+            int new_c = c + dc[i];
+            int new_cost = (i == d) ? cost[r][c][d] + 100 : cost[r][c][d] + 600;
             
-            // 벽이라면 탐색하지 않음
-            if(new_board[new_y][new_x]) {
+            if(map[new_r][new_c] == 1) {
                 continue;
             }
             
-            // 비용이 더 저렴한 경우 최소값 갱신
-            if(cost[i][new_y][new_x] > new_cost) {
-                cost[i][new_y][new_x] = new_cost;
-                q.push({new_x, new_y, new_cost, i});
+            if(cost[new_r][new_c][i] == -1 || cost[new_r][new_c][i] > new_cost) {
+                cost[new_r][new_c][i] = new_cost;
+                q.push({new_r, new_c, i});
             }
-            
         }
     }
     
-    int answer = INF;
+    int answer = numeric_limits<int>::max();
     for(int i = 0; i < 4; ++i) {
-        answer = min(answer, cost[i][N][N]);
+        if(cost[n][n][i] == -1) { 
+            continue;
+        }
+        
+        answer = (cost[n][n][i] < answer) ? cost[n][n][i] : answer;
     }
     
     return answer;
